4254becc31dc88fc8142bacd3da512e0bb9e29a9
Sergey Azimov <74828253+FarbosZ@users.noreply.github.com>
2023-03-28 18:46:08 +0300

    The app is ready

---
+++ Flash Chat iOS13/Model/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Sergey Azimov on 24.03.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message {
+    let sender: String //1@2.com
+    let body: String
+}
